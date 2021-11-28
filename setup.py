ce051a2acfa55d5cdf956c16e1af24531ebf0910
Artem-on-ishche <80548250+Artem-on-ishche@users.noreply.github.com>
2021-11-28 11:36:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='enrolment_test',
+    name='currency_telegram_bot',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
 )
